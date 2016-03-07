cf5315b6f52a306fe48c6e926e6ccfa1c2f3c218
tshiraishi <tshiraishi@mushikago.com>
2016-03-08 00:20:23 +0900

    1st commit

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.telerik.plugins.wkwebview/www/wkwebview.js",
+        "id": "com.telerik.plugins.wkwebview.wkwebview",
+        "pluginId": "com.telerik.plugins.wkwebview",
+        "clobbers": [
+            "wkwebview"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{}
+// BOTTOM OF METADATA
+});
